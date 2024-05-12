6e87513f5ba0246576ec80cac8fb7aaeb6a34484
Dylan Zammit <dylanzam@gmail.com>
2024-05-12 22:22:11 +0200

    include v2 strategy

--- traffic_sim/strategies/__init__.py
+++ traffic_sim/strategies/__init__.py
@@ -1,3 +1,4 @@
 from traffic_sim.strategies.baseline import ConstantController
 from traffic_sim.strategies.idle_switch import IdleController
 from traffic_sim.strategies.snapshot_optimiser import SnapshotController
+from traffic_sim.strategies.snapshot_optimiser_v2 import SnapshotControllerV2

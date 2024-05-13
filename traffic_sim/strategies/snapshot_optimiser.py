c3db28398781619f785964a4d08d97fda7063467
Dylan Zammit <dylanzam@gmail.com>
2024-05-13 22:51:19 +0200

    include loop_duration as parameter (def=60)

--- traffic_sim/strategies/snapshot_optimiser.py
+++ traffic_sim/strategies/snapshot_optimiser.py
@@ -6,9 +6,10 @@ from scipy.optimize import minimize
 
 class SnapshotController(Controller):
 
-    def __init__(self, rate_lookback: int = 300, **kwargs):
+    def __init__(self, rate_lookback: int = 300, loop_duration: int = 60, **kwargs):
         super().__init__(**kwargs)
         self.rate_lookback = rate_lookback
+        self.loop_duration = loop_duration
 
     def queue_penalty(self, t: list[float]):
         return sum(max(0, (lane.entry_rate_estimate - self.exit_rate * ti * 60)) ** 2 for ti, lane in zip(t, self.lanes))
@@ -32,6 +33,7 @@ class SnapshotController(Controller):
             x0 = np.array([1 / self.n_lanes] * self.n_lanes)
             bounds = [(0, 1) for _ in range(self.n_lanes)]
             cons = {'type': 'eq', 'fun': lambda x: sum(x) - 1}
+
             res = minimize(
                 self.queue_penalty,
                 x0=x0,
@@ -40,7 +42,7 @@ class SnapshotController(Controller):
             )
 
             for wait_time, lane in zip(res.x, self.lanes):
-                lane.wait_time = int(wait_time * 60)
+                lane.wait_time = int(wait_time * self.loop_duration)
 
         if self.save_hist:
             for i, lane in enumerate(self.lanes):
